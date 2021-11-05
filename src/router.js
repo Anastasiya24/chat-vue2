ec9957534c3b51afc7880169379b4b739358dfc5
Anastasiya <NastyaShugovskay24@yandex.by>
2021-11-05 17:36:13 +0300

    routing - added route restriction

--- src/router.js
+++ src/router.js
@@ -11,22 +11,36 @@ const routes = [
     path: '/',
     name: 'Chat',
     component: ChatPage,
+    // meta: {
+    //   nickName: true,
+    // },
   },
   {
     path: '/greeting',
     name: 'Greeting',
     component: GreetingPage,
+    // meta: {
+    //   nickName: false,
+    // },
   },
   {
-    path: "*",
+    path: '*',
     component: NotFoundPage,
-  }
+  },
 ];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  // TODO GET NAME
+  const isUserExist = localStorage.getItem('nickName');
+  if (to.name !== 'Greeting' && !isUserExist) next({ name: 'Greeting' });
+  if (to.name === 'Greeting' && isUserExist) next({ name: 'Chat' });
+  else next();
 });
 
 export default router;
